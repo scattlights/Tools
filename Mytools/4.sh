a4cf62759af9ca5637a3b04273c6bfb710c8aca6
scattlights <169085685+scattlights@users.noreply.github.com>
2025-06-10 10:52:58 +0800

    Update 4.sh

--- Mytools/4.sh
+++ Mytools/4.sh
@@ -3,8 +3,14 @@ clear
 echo "UFW开放指定端口"
 echo
 
+read -rp "确定要继续吗？(y/n): " confirm
+if [[ "$confirm" != "y" ]]; then
+    exit 0
+fi
+
 echo "当前已开放的端口"
 sudo ufw status
+
 while true; do
     read -rp "请输入需要开放的端口，已开放的端口无需再次输入（用英文逗号分隔，例如 22,80,443）： " ports
     if [[ $ports =~ ^[0-9]+(,[0-9]+)*$ ]]; then
@@ -12,10 +18,6 @@ while true; do
     else
         echo "输入格式错误，请使用英文逗号分隔端口"
     fi
-done
-
-for port in $(echo "$ports" | tr ',' ' '); do
-    sudo ufw allow "$port"
 done
 
 sudo ufw status
