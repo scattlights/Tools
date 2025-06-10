a728e30720e676884d33f6f9d96780a43734e6ae
scattlights <169085685+scattlights@users.noreply.github.com>
2025-06-10 09:51:49 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+while true; do
+  read -rp "请输入要执行的脚本编号： " num
+
+  if [[ ! "$num" =~ ^[1-9][0-9]*$ ]]; then
+    echo "输入错误：请输入大于0的纯数字且无空格"
+    continue
+  fi
+
+  url="https://raw.githubusercontent.com/scattlights/Tools/main/Mytools/${num}.sh"
+  bash <(curl -s "$url")
+done
