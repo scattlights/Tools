530683d6335dce0005a559d7cf68c524a3285626
scattlights <169085685+scattlights@users.noreply.github.com>
2025-06-10 10:09:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ while true; do
   read -rp "请输入要执行的脚本编号： " num
 
   if [[ ! "$num" =~ ^[1-9][0-9]*$ ]]; then
+    echo
     echo "输入错误，请输入大于0的纯数字且无空格"
     continue
   fi
@@ -14,6 +15,7 @@ while true; do
   if curl --silent --head --fail "$url" > /dev/null; then
     bash <(curl -s "$url")
   else
+    echo
     echo "未找到编号为 $num 的脚本"
   fi
 done
